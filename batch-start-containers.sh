e7d0db3a0d514299e68395c3c34c183baf07480f
Leonardo Noleto <noleto.leonardo@gmail.com>
2016-02-07 17:27:24 +0100

    Limit docker memory when launching a lot of them.

--- batch-start-containers.sh
+++ batch-start-containers.sh
@@ -13,5 +13,5 @@ do
 	city_name="${c_names[$i]}"
 	web_port=$((base_web_port + $i))
 	spark_ui_port=$((base_spark_ui_port + $i))
-	docker run -d -p $web_port:8888 -p $spark_ui_port:4040 --name $city_name noleto/pyspark-jupyter
+	docker run -m 3G -d -p $web_port:8888 -p $spark_ui_port:4040 --name $city_name noleto/pyspark-jupyter
 done
